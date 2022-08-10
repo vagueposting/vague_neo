dcb097ec079e9989d75070403e6436496dd9fe4c
vagueposting <kaila_velasco@dlsu.edu.ph>
2022-08-10 17:15:21 +0800

    Delete indexhide.js

--- public/js/indexhide.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('input[type="radio"]').click(function(){
-    	var demovalue = $(this).val();
-        $("div.myDiv").hide();
-        $("#index"+demovalue).show();
-    });
-});
